3ef5cad9fffe1f839aa2605e28ed7a84596a7f78
kar61614 <112227050+kar61614@users.noreply.github.com>
2023-08-06 11:47:15 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+function preload(){
+    img = loadImage('dog_cat.jpg');
+  }
+  
+  
+  function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+  }
+  
+  
+  function draw() {
+    image(img, 0, 0, 640, 420);
+    fill(255, 0, 0);
+    text("Dog", 45, 75);
+    noFill();
+    stroke(255, 0, 0);
+    rect(30, 60, 450, 350 );
+  }
